ce8031b9d027f80d3bf7fd2b9de0f2a33a1fadd4
Joseph Chen <zychen.uestc@gmail.com>
2020-12-25 10:34:03 +0800

    update CMakeLists.txt

--- py/setup.py
+++ py/setup.py
@@ -42,30 +42,107 @@ def which(program):
                 return exe_file
 
     return None
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
+
+CMAKE_PREFIX = '/workspace/TRTorch/libtorch/;/workspace/TensorRT-7.0.0.11/'
+
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 
-BAZEL_EXE = which("bazel")
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cfg = "Debug" if self.debug else "Release"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+            "-DCMAKE_PREFIX_PATH={}".format(CMAKE_PREFIX)
+        ]
+        build_args = []
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            try:
+                import ninja
+                if not cmake_generator:
+                    cmake_args += ["-GNinja"]
+            except:
+                pass
+
+        else:
+
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+                ]
+                build_args += ["--config", cfg]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += ["-j{}".format(self.parallel)]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
 
 
 def build_libtrtorch_pre_cxx11_abi(develop=True, use_dist_dir=True, cxx11_abi=False):
-    cmd = [BAZEL_EXE, "build"]
-    cmd.append("//cpp/api/lib:libtrtorch.so")
-    if develop:
-        cmd.append("--compilation_mode=dbg")
-    else:
-        cmd.append("--compilation_mode=opt")
-    if use_dist_dir:
-        cmd.append("--distdir=third_party/dist_dir/x86_64-linux-gnu")
-    if not cxx11_abi:
-        cmd.append("--config=python")
-    else:
-        print("using CXX11 ABI build")
-
-    print("building libtrtorch")
-    status_code = subprocess.run(cmd).returncode
-
-    if status_code != 0:
-        sys.exit(status_code)
+    pass
 
 
 def gen_version_file():
@@ -82,10 +159,7 @@ def copy_libtrtorch(multilinux=False):
         os.makedirs(dir_path + '/trtorch/lib')
 
     print("copying library into module")
-    if multilinux:
-        copyfile(dir_path + "/build/libtrtorch_build/libtrtorch.so", dir_path + '/trtorch/lib/libtrtorch.so')
-    else:
-        copyfile(dir_path + "/../bazel-bin/cpp/api/lib/libtrtorch.so", dir_path + '/trtorch/lib/libtrtorch.so')
+    copyfile(os.path.join(dir_path, "../build/libtrtorch.so"), dir_path + '/trtorch/lib/libtrtorch.so')
 
 
 class DevelopCommand(develop):
@@ -173,12 +247,12 @@ ext_modules = [
             'trtorch/csrc/tensorrt_classes.cpp',
             'trtorch/csrc/register_tensorrt_classes.cpp',
         ],
-        library_dirs=[(dir_path + '/trtorch/lib/'), "/opt/conda/lib/python3.6/config-3.6m-x86_64-linux-gnu"],
+        library_dirs=[(dir_path + '/trtorch/lib/'), "/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/"], 
         libraries=["trtorch"],
         include_dirs=[
             dir_path + "trtorch/csrc",
             dir_path + "/../",
-            dir_path + "/../bazel-TRTorch/external/tensorrt/include",
+            dir_path + "/tensorrt/include",
         ],
         extra_compile_args=[
             "-Wno-deprecated",
@@ -203,8 +277,9 @@ setup(name='trtorch',
       long_description_content_type='text/markdown',
       long_description=long_description,
       ext_modules=ext_modules,
+      #ext_modules=[CMakeExtension("trtorch/csrc")],
       install_requires=[
-          'torch==1.7.1',
+          'torch==1.6.0',
       ],
       setup_requires=[],
       cmdclass={
@@ -212,6 +287,7 @@ setup(name='trtorch',
           'clean': CleanCommand,
           'develop': DevelopCommand,
           'build_ext': cpp_extension.BuildExtension,
+          #'build_ext': CMakeBuild, 
           'bdist_wheel': BdistCommand,
       },
       zip_safe=False,
